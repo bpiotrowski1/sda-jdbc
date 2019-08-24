a4917ac303a70438ad1306e0e41a5e21de676070
bpiotrowski1 <baej.p26@gmail.com>
2019-08-24 14:16:57 +0200

    solid

--- src/main/java/sda/jdbc/zadania/zadanie3/DataAccessObject.java
+++ src/main/java/sda/jdbc/zadania/zadanie3/DataAccessObject.java
@@ -3,8 +3,12 @@ package sda.jdbc.zadania.zadanie3;
 import java.sql.*;
 
 class DataAccessObject {
+    private String url = DBProperties.getUrl();
+    private String user = DBProperties.getUser();
+    private String password = DBProperties.getPassword();
+
     Ksiazka findById(int id) {
-        try(Connection connection = DriverManager.getConnection(DBProperties.getUrl(), DBProperties.getUser(), DBProperties.getPassword())) {
+        try(Connection connection = DriverManager.getConnection(url, user, password)) {
             final String sqlSelectById = "SELECT * FROM ksiazka WHERE id=?";
             PreparedStatement preparedStatement = connection.prepareStatement(sqlSelectById);
             preparedStatement.setInt(1, id);
@@ -18,7 +22,7 @@ class DataAccessObject {
     }
 
     void newBooks(String tytul) {
-        try(Connection connection = DriverManager.getConnection(DBProperties.getUrl(), DBProperties.getUser(), DBProperties.getPassword())) {
+        try(Connection connection = DriverManager.getConnection(url, user, password)) {
             final String sqlInsert = "INSERT INTO ksiazka(tytul) VALUES (?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert);
             preparedStatement.setString(1, tytul);
@@ -29,7 +33,7 @@ class DataAccessObject {
     }
 
     void updateById(int id, String tytul) {
-        try(Connection connection = DriverManager.getConnection(DBProperties.getUrl(), DBProperties.getUser(), DBProperties.getPassword())) {
+        try(Connection connection = DriverManager.getConnection(url, user, password)) {
             final String sqlUpdate = "UPDATE ksiazka SET tytul=? WHERE id=?";
             PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate);
             preparedStatement.setString(1, tytul);
@@ -41,7 +45,7 @@ class DataAccessObject {
     }
 
     void deleteById(int id) {
-        try(Connection connection = DriverManager.getConnection(DBProperties.getUrl(), DBProperties.getUser(), DBProperties.getPassword())) {
+        try(Connection connection = DriverManager.getConnection(url, user, password)) {
             final String sqlDelete = "DELETE FROM ksiazka WHERE id=?";
             PreparedStatement preparedStatement = connection.prepareStatement(sqlDelete);
             preparedStatement.setInt(1, id);
@@ -52,7 +56,7 @@ class DataAccessObject {
     }
 
     void getAllBooks() {
-        try(Connection connection = DriverManager.getConnection(DBProperties.getUrl(), DBProperties.getUser(), DBProperties.getPassword())) {
+        try(Connection connection = DriverManager.getConnection(url, user, password)) {
             final String sqlSelectAll = "SELECT * FROM ksiazka";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlSelectAll);
@@ -66,7 +70,7 @@ class DataAccessObject {
     }
 
     int getLastId() {
-        try(Connection connection = DriverManager.getConnection(DBProperties.getUrl(), DBProperties.getUser(), DBProperties.getPassword())) {
+        try(Connection connection = DriverManager.getConnection(url, user, password)) {
             final String sqlSelectLastId = "SELECT max(id) FROM ksiazka";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlSelectLastId);
